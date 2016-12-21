7d10473f3b0d61857a4b9c4b980b8f9783a43c47
shiai8901 <shiai8901@gmail.com>
2016-12-20 16:27:54 -0800

    Refactor using ES6 classes

--- src/floatingAwayDancer.js
+++ src/floatingAwayDancer.js
@@ -1,32 +1,23 @@
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
+// var makeFloatingAwayDancer = function(top, left, timeBetweenSteps) {
+//   makeDancer.call(this, top, left, timeBetweenSteps);
+//   this.$node.addClass('fly');
 // };
 
-var makeFloatingAwayDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node.addClass('fly');
-};
+// makeFloatingAwayDancer.prototype = Object.create(makeDancer.prototype);
+// makeFloatingAwayDancer.prototype.constructor = makeFloatingAwayDancer;
 
-makeFloatingAwayDancer.prototype = Object.create(makeDancer.prototype);
-makeFloatingAwayDancer.prototype.constructor = makeFloatingAwayDancer;
+// makeFloatingAwayDancer.prototype.step = function() {
+//   makeDancer.prototype.step.call(this); 
+//   this.$node.css({top: '-=30px'});
+// };
 
-makeFloatingAwayDancer.prototype.step = function() {
-  makeDancer.prototype.step.call(this); //oldStep
-  this.$node.css({top: '-=30px'});
+var makeFloatingAwayDancer = class MakeFloatingAwayDancer extends makeDancer {
+  constructor (top, left, timeBetweenSteps) {
+    super(top, left, timeBetweenSteps);
+    this.$node.addClass('fly');
+  }
+  step () {
+    super.step();
+    this.$node.css({top: '-=30px'});
+  }
 };
